cc6b26c3b61a85a1ff7436fcb8ad95aa310950fb
Karthik <tarunkarthik999@gmail.com>
2018-02-23 16:04:53 +0530

    Corrected Errors

--- ITA_V30/app/src/main/java/com/example/abc/ita_v30/ChoosefromGallary.java
+++ ITA_V30/app/src/main/java/com/example/abc/ita_v30/ChoosefromGallary.java
@@ -1,7 +1,10 @@
 package com.example.abc.ita_v30;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +12,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class ChoosefromGallary extends AppCompatActivity implements View.OnClickListener {
 
     private static final int SELECT_PICTURE = 100;
     private static final String TAG = "MainActivity";
+    InputStream image_stream;
+    String datapath = "";
+
+    Bitmap bitmap;
+    private TessBaseAPI mTess;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +61,71 @@ public class ChoosefromGallary extends AppCompatActivity implements View.OnClick
                     // Set the image in ImageView
                    ((ImageView) findViewById(R.id.imgView)).setImageURI(selectedImageUri);
                 }
+                try {
+                    image_stream = getContentResolver().openInputStream(selectedImageUri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                bitmap = BitmapFactory.decodeStream(image_stream);
+                String language = "eng";
+                datapath = getFilesDir()+ "/tesseract/";
+                mTess = new TessBaseAPI();
+
+                checkFile(new File(datapath + "tessdata/"));
+
+                mTess.init(datapath, language);
             }
+        }
+    }
+    public void processImage(View view){
+        String OCRresult = null;
+        mTess.setImage(bitmap);
+        OCRresult = mTess.getUTF8Text();
+        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+        OCRTextView.setText(OCRresult);
+    }
+
+    private void checkFile(File dir) {
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFiles();
+        }
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+    }
+
+    private void copyFiles() {
+        try {
+            String filepath = datapath + "/tessdata/eng.traineddata";
+            AssetManager assetManager = getAssets();
+
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+
+
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+            File file = new File(filepath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
